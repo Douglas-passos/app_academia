4498f87248a4856d159ea216d5cf8ec1a4324cac
Douglas <douglaspassos_contato@hotmail.com>
2025-03-15 10:16:13 -0300

    Segunda Commit

--- app_academia/urls.py
+++ app_academia/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('cadastrar', views.cadastrar, name='cadastrar'),
     path('treinos', views.treinos, name='treinos'),
+    path('listar', views.listar, name='listar'),
+    path('editar/<int:id>', views.editar, name='editar'),
+    path('excluir/<int:id>', views.excluir, name='excluir'),
 ]
--- app_academia/views.py
+++ app_academia/views.py
@@ -11,6 +11,7 @@ def home(request):
 def treinos(request):
     return render(request, 'app_academia/treinos.html')
 
+
 def cadastrar(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
@@ -26,7 +27,30 @@ def cadastrar(request):
 
         # Criar novo usuário
         usuario = Cadastrar.objects.create(nome=nome, email=email, senha=senha_hashed)
-        
-        return redirect('home')
-    
+        return redirect('/')
     return render(request, 'app_academia/cadastrar.html')
+
+def listar(request):
+    usuarios = Cadastrar.objects.all()
+    return render(request, 'app_academia/listar.html', {'usuarios': usuarios})
+
+def editar(request, id):
+    usuario = Cadastrar.objects.get(id=id)
+    if request.method == 'POST':
+        usuario.nome = request.POST.get('nome')
+        usuario.email = request.POST.get('email')
+        usuario.senha = request.POST.get('senha')
+        usuario.save()
+        return redirect('listar')
+    return render(request, 'app_academia/editar.html', {'usuario': usuario})
+
+def excluir(request, id):
+    usuario = Cadastrar.objects.get(id=id)
+    usuario.delete()
+    return redirect('listar')
+
+def loguin(request):
+    return render(request, 'app_academia/home.html')
+
+
+    
